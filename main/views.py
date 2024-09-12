81ee167950fd49e73ce7216a9138bcbf61645b5b
akshaykskrn <134961550+akshaykskrn@users.noreply.github.com>
2024-09-12 16:02:59 +0530

    chane

--- main/views.py
+++ main/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Employee
 from .forms import Employeeform
 
-# Create your views here.
 def employee_list(request):
     employees = Employee.objects.all()
     return render(request, 'emp_list.html', {'employees': employees})
@@ -12,8 +11,25 @@ def add_employee(request):
         form = Employeeform(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('emp_list')  # Ensure this matches the name in your URL patterns
+            return redirect('emp_list')
     else:
         form = Employeeform()
-    
     return render(request, 'add_emp.html', {'form': form})
+
+def delete_emp(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('emp_list')
+    return render (request, 'delete_emp.html', {'employee': employee})
+
+def update_emp(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    if request.method == 'POST':
+        form = Employeeform(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('emp_list')
+    else:
+        form = Employeeform(instance=employee)
+    return render(request, 'update_emp.html', {'form': form})
